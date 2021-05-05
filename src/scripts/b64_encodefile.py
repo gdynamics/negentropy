2afcbc261b515efcaf3a9d7800f3f48ac8b4a2c4
gdynamics <coopervk@gmail.com>
2021-05-05 01:00:46 -0500

    Add encryption support, byte string reading, and mac(h)beth typo

--- src/scripts/b64_encodefile.py
+++ src/scripts/b64_encodefile.py
@@ -6,16 +6,31 @@ from b64 import B64
 
 if __name__ == "__main__":
     print('-'*90)
+    print('Unencrypted sample file macbeth.txt...')
 
     print('Reading ../test/macbeth.txt')
-    with open('../test/macbeth.txt', 'r') as f:
+    with open('../test/macbeth.txt', 'rb') as f:
         data = f.read()
 
     print('Converting data to base64 with padding')
     data = B64.encode(data)
 
     print('Writing base64 data to ../test/macbeth.txt.b64')
-    with open('../test/machbeth.txt.b64', 'w+') as f:
+    with open('../test/macbeth.txt.b64', 'w+') as f:
+        f.write(data)
+
+    print('-'*90)
+    print('Encrypted sample file macbeth.txt.enc...')
+    
+    print('Reading ../test/macbeth.txt.enc')
+    with open('../test/macbeth.txt.enc', 'rb') as f:
+        data = f.read()
+
+    print('Converting data to base64 with padding')
+    data = B64.encode(data)
+
+    print('Writing base64 data to ../test/macbeth.txt.enc.b64')
+    with open('../test/macbeth.txt.enc.b64', 'w+') as f:
         f.write(data)
 
     print('-'*90)
